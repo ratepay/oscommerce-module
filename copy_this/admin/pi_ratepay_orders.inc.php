4a5639b393141007852d2c5eadd065acf7c2e346
Aarne Welschlau <aarne.welschlau@ratepay.com>
2014-09-03 16:06:25 +0200

    cleaning the branch

--- copy_this/admin/pi_ratepay_orders.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-if($oInfo->payment_method == "RatePAY Rate") {
-	include 'pi_ratepay_orders_rate.inc.php';
-} else if($oInfo->payment_method == "RatePAY Rechnung") {
-	include 'pi_ratepay_orders_rechnung.inc.php';
-}
-?>
